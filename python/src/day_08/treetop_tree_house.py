7d078d010a4bbb680d0bcf7e096276f58e7e7faf
Benjamin Andre <benjaminandre23@gmail.com>
2023-11-09 21:26:28 +0100

    improved day 8

--- python/src/day_08/treetop_tree_house.py
+++ python/src/day_08/treetop_tree_house.py
@@ -1,98 +1,38 @@
 import time
-from dataclasses import dataclass
 
-@dataclass(frozen=True)
-class Tree():
-    """ Tree class, has coordinates of it's place in the forest (x,y)"""
-    x: int
-    y: int
-
-class Forest():
-    
-    def __init__(self, data: list) -> None:
-        """ Expects data as a list of tree rows :
-            [[1, 2, 3, 4, 5], [6, 7, 8, 9, 0], ...]"""
-        self.rows = data
-        self.cols = list(zip(*self.rows))
-        self._width = len(self.cols)
-        self._height = len(self.rows)
-        self.size = self._width * self._height
-
-    def height_of_tree(self, tree: Tree) -> int:
-        return self.rows[tree.y][tree.x]
-
-    def is_visible(self, tree: Tree) -> int:
-        """ Check if a tree is visible from any edge of the forest"""
-
-        if tree.x == 0 or tree.x == self._width - 1:
-            return True
-        if tree.y == 0 or tree.y == self._height - 1:
-            return True
-
-        height = self.height_of_tree(tree)
-
-        if height > max(self.rows[tree.y][0:tree.x]): return True
-        if height > max(self.rows[tree.y][tree.x + 1:]): return True
-        if height > max(self.cols[tree.x][0:tree.y]): return True
-        if height > max(self.cols[tree.x][tree.y + 1:]): return True
-
-        return False
-
-    def get_visible_trees(self) -> set[Tree]:
-        return { Tree(x, y) for x in range(self._height)
-                            for y in range(self._width)
-                            if self.is_visible(Tree(x, y))}
-
-    def best_scenic_score(self) -> int:
-        """ Return the best scenic score in the forest"""
-        best = 0
-        for y in range(self._width):
-            for x in range(self._height):
-                tree = Tree(x, y)
-                score = self.get_tree_scenic_score(tree)
-                if score > best:
-                    best = score
-
-        return best
-
-    def get_tree_scenic_score(self, tree: Tree) -> int:
-        """ Returns the scenic score of a given tree in the forest"""
-
-        height = self.height_of_tree(tree)
-
-        right = (x for x in self.rows[tree.y][tree.x+1:])
-        bottom = (y for y in self.cols[tree.x][tree.y+1:])
-        left = (x for x in reversed(self.rows[tree.y][0:tree.x]))
-        top = (x for x in reversed(self.cols[tree.x][0:tree.y]))
-
-        res = 1
-        for direction in (right, bottom, left, top):
-            visible_trees = 0
-            for t_height in direction:
-                if t_height < height:
-                    visible_trees += 1
-                else:
-                    visible_trees += 1
-                    break
-            res *= visible_trees
-
-        return res
-
-
-
-        
-
+def examine_tree(grid, r, c):
+    tree = grid[r][c]
+    invisible, score = True, 1
+    directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+    for dr, dc in directions:
+        i, visible = 0, True
+        row, col = r, c
+        while 0 <= row + dr < len(grid) and 0 <= col + dc < len(grid[0]):
+            next_value = grid[row + dr][col + dc]
+            i += 1
+            if tree <= next_value:
+                visible = False
+                break
+            row += dr
+            col += dc
+        if visible:
+            invisible = False
+        score *= i
+    return not invisible, score
 
 def main():
     with open("input/input.txt", "r") as f:
-        data = f.read().splitlines()
+        data = [list(map(int, line)) for line in f.read().splitlines()]
 
-    forest = Forest(data)
-    visible = forest.get_visible_trees()
-    print(f"Part 1: Number of visible trees : {len(visible)}")
-    scenic_score = forest.best_scenic_score()
-    print(f"Part 2: Best scenic score of the forest : {scenic_score}")
 
+    result = (0, 0)
+    for r in range(len(data)):
+        for c in range(len(data[0])):
+            visible, score = examine_tree(data, r, c)
+            result = (result[0] + int(visible), max(result[1], score))
+
+    print(f"Part 1: Number of visible trees : {result[0]}")
+    print(f"Part 2: Best scenic score of the forest : {result[1]}")
     
 
 if __name__ == "__main__":
