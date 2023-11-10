276f1847eb1b56e70b4409e7d5f69595e868973d
Benjamin Andre <benjaminandre23@gmail.com>
2023-11-10 15:58:45 +0100

    improved day 12

--- python/src/day_12/hill_climbing_algorithm.py
+++ python/src/day_12/hill_climbing_algorithm.py
@@ -1,7 +1,5 @@
 import time
-from queue import Queue
-
-DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+from collections import deque
 
 def find_pos(data, val):
     """ Finds the position of the value val in data"""
@@ -19,57 +17,43 @@ def find_all_pos(data, val):
                 positions.append((i,j))
     return positions
 
+def bfs(grid, start, goal):
+    rows, cols = len(grid), len(grid[0])
+    visited = [[False] * cols for _ in range(rows)]
+    queue = deque((x, y, 0) for x, y in start)
 
-def valid_direction(data, pos, length):
-    """ Finds the valid directions to go from position pos in data of length i*j"""
-    valid = []
-    pos_data = ord(data[pos[0]][pos[1]])
-    for direction in DIRECTIONS:
-        cur = pos[0] + direction[0], pos[1] + direction[1]
-        if 0 <= cur[0] < length[0] and 0 <= cur[1] < length[1]:
-            cur_data = ord(data[cur[0]][cur[1]])
-            if cur_data <= pos_data + 1 :
-                valid.append(cur)
-    return valid
+    while queue:
+        x, y, length = queue.popleft()
 
-def bfs(data, start, end):
-    data_len = len(data), len(data[0])
-    queue = Queue()
-    queue.put((start[0], start[1], 0))
-    visited = [[False for _ in range(data_len[1])] for _ in range(data_len[0])]
-    data[start[0]][start[1]] = 'a'
-    data[end[0]][end[1]] = 'z'
-
-    while queue.qsize() > 0:
-        # Get the next node to visit and return if it's the end
-        i, j, length = queue.get()
-        if (i, j) == end:
+        if (x, y) == goal:
             return length
-        
-        # Visit the node
-        visited[i][j] = True
-        length += 1
-        # See what node to visit next
-        for x,y in valid_direction(data, (i,j), data_len):
-            if not visited[x][y] and (x,y) not in [(l[0],l[1]) for l in list(queue.queue)]:
-                queue.put((x, y, length)) 
 
-    return 530
+        for dx, dy in [(0, -1), (-1, 0), (0, 1), (1, 0)]:
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < rows and 0 <= ny < cols:
+                square = ord(grid[nx][ny])
+                if ord(grid[x][y]) + 1 >= square and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny, length + 1))
+
+    return None
 
 def main():
     with open("input/input.txt", "r") as f:
-        data = [list(line) for line in f.read().splitlines()]
+        grid = [list(line) for line in f.read().splitlines()]
 
-    start = find_pos(data, 'S')
-    end = find_pos(data, 'E')
+    sx, sy = find_pos(grid, 'S')
+    ex, ey = find_pos(grid, 'E')
 
-    length = bfs(data, start, end)
-    print(f"Part 1 : Fewest possible steps : {length}")
+    grid[sx][sy] = 'a'
+    grid[ex][ey] = 'z'
 
-    positions = find_all_pos(data, 'a')
-    all_length = [bfs(data, (i,j), end) for i,j in positions]
-    print(f"Part 2 : Fewest possible tests puissance 9000 : {min(all_length)}")
-
+    positions = [(x, y) for x in range(len(grid)) for y in range(len(grid[0])) if grid[x][y] == 'a']
+    res1 = bfs(grid, [(sx, sy)], (ex, ey))
+    res2 = bfs(grid, positions, (ex, ey))
+    print(f"Part 1: Fewest possible steps : {res1}")
+    print(f"Part 2: Fewest possible steps on coke : {res2}")
 
 if __name__ == "__main__":
     t1 = time.perf_counter()
